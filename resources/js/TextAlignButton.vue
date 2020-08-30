b6869e4c5df2e4eb441da7c02eb2efd81d48d3f9
ssatz <sathish.thi@gmail.com>
2020-08-30 13:48:15 +0530

    :bug: fixed active text align selection
    Signed-off-by: ssatz <sathish.thi@gmail.com>

---
+++ resources/js/TextAlignButton.vue
@@ -0,0 +1,36 @@
+<!--
+  - /**
+  -  *  * Copyright (C) optimoapps.com - All Rights Reserved
+  -  *  * Unauthorized copying of this file, via any medium is strictly prohibited
+  -  *  * Proprietary and confidential
+  -  *  * Written by Sathish Kumar(satz) <sathish.thi@gmail.com>, ManiKandan<smanikandanit@gmail.com >
+  -  *
+  -  */
+  -->
+
+<template>
+  <div class="inline-block relative">
+    <button
+        class="bard-toolbar-button"
+        :class="{ 'active': getMarkAttrs('textAlign').align === this.button.args.align }"
+        v-html="button.html"
+        v-tooltip="button.text"
+        @click="setAlignment()"
+    />
+  </div>
+</template>
+<script>
+export default {
+  mixins: [BardToolbarButton],
+  data() {
+    return {
+      getMarkAttrs: this.editor.getMarkAttrs.bind(this.editor),
+    }
+  },
+  methods: {
+    setAlignment() {
+      this.editor.commands.textAlign({align: this.button.args.align});
+    },
+  }
+}
+</script>
